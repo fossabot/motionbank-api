d31a3bd24210cee1b2535eb56603815064487d78
A. Koch <dasantonym@users.noreply.github.com>
2018-03-03 18:26:39 +0100

    updated annotation data type, added nested schemas

--- dist/resources/annotation.js
+++ dist/resources/annotation.js
@@ -11,13 +11,46 @@ const resourceHooks = {},
       schemaOptions = { idField: 'uuid'
 
   /**
-   * Annotation Schema
+   * Selector Schema
    * @type {SchemaObjectInstance<any>}
    */
-};const Schema = (0, _base.initSchema)({
+};const Selector = (0, _base.initSchema)({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
+  conformsTo: { type: String }
+});
+
+/**
+ * Body Schema
+ * @type {SchemaObjectInstance<any>}
+ */
+const Body = (0, _base.initSchema)({
+  type: { type: String, required: true },
+  value: { type: String },
+  source: { type: String },
+  purpose: { type: String },
+  selector: { type: Selector }
+});
+
+/**
+ * Target Schema
+ * @type {SchemaObjectInstance<any>}
+ */
+const Target = (0, _base.initSchema)({
+  type: { type: String, required: true },
+  id: { type: String, required: true },
+  selector: { type: Selector }
+});
+
+/**
+ * Annotation Schema
+ * @type {SchemaObjectInstance<any>}
+ */
+const Schema = (0, _base.initSchema)({
+  type: { type: String, required: true, default: 'Annotation', readonly: true },
   author: { type: String, required: true },
-  target: { type: String, required: true },
-  body: { type: String, required: true },
+  target: { type: Target, required: true },
+  body: { type: Body, required: true },
   motivation: { type: String },
   context: { type: String }
 }, schemaOptions);
--- src/resources/annotation.js
+++ src/resources/annotation.js
@@ -2,13 +2,46 @@ import { initSchema } from '../base'
 const resourceHooks = {}, schemaOptions = { idField: 'uuid' }
 
 /**
+ * Selector Schema
+ * @type {SchemaObjectInstance<any>}
+ */
+const Selector = initSchema({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
+  conformsTo: { type: String }
+})
+
+/**
+ * Body Schema
+ * @type {SchemaObjectInstance<any>}
+ */
+const Body = initSchema({
+  type: { type: String, required: true },
+  value: { type: String },
+  source: { type: String },
+  purpose: { type: String },
+  selector: { type: Selector }
+})
+
+/**
+ * Target Schema
+ * @type {SchemaObjectInstance<any>}
+ */
+const Target = initSchema({
+  type: { type: String, required: true },
+  id: { type: String, required: true },
+  selector: { type: Selector }
+})
+
+/**
  * Annotation Schema
  * @type {SchemaObjectInstance<any>}
  */
 const Schema = initSchema({
+  type: { type: String, required: true, default: 'Annotation', readonly: true },
   author: { type: String, required: true },
-  target: { type: String, required: true },
-  body: { type: String, required: true },
+  target: { type: Target, required: true },
+  body: { type: Body, required: true },
   motivation: { type: String },
   context: { type: String }
 }, schemaOptions)
